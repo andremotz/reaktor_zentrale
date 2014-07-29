480a96d318b99f030a3ec6af1feb8b2a144dffff
andremotz <me@andremotz.com>
2014-07-29 22:09:00 +0200

    update

--- reaktor/src/com/andremotz/reaktor/servlets/ReaktorSessionService.java
+++ reaktor/src/com/andremotz/reaktor/servlets/ReaktorSessionService.java
@@ -2,9 +2,11 @@ package com.andremotz.reaktor.servlets;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 import com.andremotz.reaktor.dao.Calculator;
 import com.andremotz.reaktor.dao.DatabaseHandler;
@@ -13,49 +15,52 @@ import com.andremotz.reaktor.dao.GlobalData;
 import com.andremotz.reaktor.dao.Zielwert;
 
 /**
- * A simple CDI service which is able to say hello to someone
+ * A simple CDI service
  * 
- * @author Pete Muir
  * 
  */
-public class HelloService {
-	
+public class ReaktorSessionService {
+
+	private String scriptpath;
 	FileHandler fileHandler;
 	DatabaseHandler dbHandler;
 	Calculator calculator;
-	
+
 	private int globalTakt = 5;
-	
+
 	public int getGlobalTakt() {
 		return globalTakt;
 	}
 
 	GlobalData globalData;
 	ArrayList<Zielwert> zielwerte;
-	
+
 	Float tempInnen;
 	Float tempAussen;
 	int currentZielTemp;
 	private boolean isHeating;
-	
+	private String fileSensorsAverage;
+
 	public boolean getIsHeating() {
 		return isHeating;
 	}
-	
+
 	public int getCurrentZieltemp() {
 		return currentZielTemp;
 	}
 
-	HelloService() {
+	ReaktorSessionService() {
+		getProperties();
+		
 		dbHandler = new DatabaseHandler();
 		fileHandler = new FileHandler();
 		calculator = new Calculator();
-		
+
 		tempInnen = 0f;
 		tempAussen = 0f;
-		
+
 		isHeating = false;
-		
+
 		globalData = dbHandler.getGlobalData();
 		zielwerte = dbHandler.getZielwerte();
 	}
@@ -69,54 +74,86 @@ public class HelloService {
 		this.globalData = globalData;
 	}
 
-
-	String createHelloMessage(String name) {
-		return "Hey " + name + "!!!!! : )";
+	String getSensorsAverage() {
+		return fileHandler.getContentfromFile(fileSensorsAverage);
 	}
 
-	String getSensorsAverage() {
-		// TODO Pfad relativ
-//		String filename = "/home/pi/sensordata/sensorsAverages.temp";
-		String filename = "/Users/andremotz/Desktop/sensorsAverages.temp";
-		return fileHandler.getContentfromFile(filename);
+	/*
+	 * Get properties from Property-file
+	 */
+	private void getProperties() {
+
+		Properties prop = new Properties();
+		InputStream input = null;
+
+		try {
+
+			String filename = "config.properties";
+			input = ReaktorSessionService.class.getClassLoader().getResourceAsStream(
+					filename);
+			if (input == null) {
+				System.out.println("Sorry, unable to find " + filename);
+				// return null;
+			}
+
+			// load a properties file from class path, inside static method
+			prop.load(input);
+
+			// get the property values
+			scriptpath = prop.getProperty("scriptpath");
+			fileSensorsAverage = prop.getProperty("fileSensorsAverage");
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
 	}
 
 	public List<Float> getSensorsTemps() {
 		List<Float> sensorsTemps = new ArrayList<Float>();
-		
+
 		List<Float> sensorsValues = new ArrayList<Float>();
-		
-			try {
-				sensorsValues = fileHandler
-						.getSensorValuesAsArrayList(getSensorsAverage());
-			} catch (NumberFormatException e) {
-				sensorsValues.add(0f);
-				
-			}
-			// Wenn Sensorwerte nicht ausgelsesn werden k??nnen
-			if (sensorsValues.size() != 2) {
-				sensorsTemps.add(0f);
-				sensorsTemps.add(0f);
-				isHeating = false;
-			} else {
-				sensorsTemps = calculator.convertSensorValuesToCelsius(sensorsValues);
-				
-			}
+
+		try {
+			sensorsValues = fileHandler
+					.getSensorValuesAsArrayList(getSensorsAverage());
+		} catch (NumberFormatException e) {
+			sensorsValues.add(0f);
+
+		}
+		// Wenn Sensorwerte nicht ausgelsesn werden k??nnen
+		if (sensorsValues.size() != 2) {
+			sensorsTemps.add(0f);
+			sensorsTemps.add(0f);
+			isHeating = false;
+		} else {
+			sensorsTemps = calculator
+					.convertSensorValuesToCelsius(sensorsValues);
+
+		}
 		return sensorsTemps;
 	}
-	
+
 	private void switchArduino() {
 		try {
 			Runtime rt = Runtime.getRuntime(); // step 1
-			
-			String scriptPath;
+
+			String script;
 			if (isHeating) {
-				scriptPath = "/home/pi/sensordata/switchArduinoOn.sh";
+				script = "switchArduinoOn.sh";
 			} else {
-				scriptPath = "/home/pi/sensordata/switchArduinoOff.sh";
+				script = "switchArduinoOff.sh";
 			}
-				
-			Process process = rt.exec(scriptPath); // step 2
+
+			Process process = rt.exec(scriptpath + "/" + script); // step 2
 
 			InputStreamReader reader = // step 3
 			new InputStreamReader(process.getInputStream());
@@ -133,22 +170,22 @@ public class HelloService {
 	}
 
 	public void circuitGo() {
-	
-		if(globalData.getIsRunning()) {
-			 
+
+		if (globalData.getIsRunning()) {
+
 			// wenn secondsCompleted mehr ist als gesamt, dann aus
-			
+
 			int secondsCompletedGesamt = 0;
 			for (int i = 0; i < zielwerte.size(); i++) {
-				secondsCompletedGesamt = secondsCompletedGesamt + 
-						zielwerte.get(i).getSeconds();
+				secondsCompletedGesamt = secondsCompletedGesamt
+						+ zielwerte.get(i).getSeconds();
 			}
-			
+
 			if (globalData.getSecondsCompleted() > secondsCompletedGesamt) {
 				isHeating = false;
 				switchArduino();
 			}
-			
+
 			try {
 				// wenn tempInnen < currentZieltemp + 2, dann ein
 				tempAussen = getSensorsTemps().get(0);
@@ -156,76 +193,84 @@ public class HelloService {
 			} catch (IndexOutOfBoundsException e) {
 				tempAussen = 0f;
 				tempInnen = 0f;
-				
+
 			}
-			
+
 			int secondsCompleted = globalData.getSecondsCompleted();
-			int currentLevel = getLevelFromSecondsCompleted(secondsCompleted, zielwerte);
+			int currentLevel = getLevelFromSecondsCompleted(secondsCompleted,
+					zielwerte);
 			currentZielTemp = getCelsiusFromLevel(currentLevel, zielwerte);
-			
+
 			if (tempAussen != 0) {
 				if (tempAussen < currentZielTemp + 2) {
 					isHeating = true;
 				}
-				
+
 				// Sind wir im gr??nen Bereich?
-				if (tempAussen > currentZielTemp - 2 && tempAussen < currentZielTemp + 10) {
-					int newSecondsCompleted = dbHandler.getGlobalData().getSecondsCompleted() + globalTakt;
+				if (tempAussen > currentZielTemp - 2
+						&& tempAussen < currentZielTemp + 10) {
+					int newSecondsCompleted = dbHandler.getGlobalData()
+							.getSecondsCompleted() + globalTakt;
 					globalData.setSecondsCompleted(newSecondsCompleted);
 					dbHandler.setSecondsCompleted(newSecondsCompleted);
 				}
 			}
-			
+
 		} else {
 			isHeating = false;
 		}
-		
+
 		switchArduino();
-		
+
 		String timestamp = calculator.getCurrentTimestamp();
-		
+
 		// Logging
-		dbHandler.saveSensorDataToDB(timestamp, tempAussen, tempInnen, 
+		dbHandler.saveSensorDataToDB(timestamp, tempAussen, tempInnen,
 				globalData.getIsRunning(), isHeating, currentZielTemp);
-		
+
 	}
-	
+
 	private int getCelsiusFromLevel(int level, ArrayList<Zielwert> zielwerte) {
 		return zielwerte.get(level).getCelsius();
-		
+
 	}
 
-	int getLevelFromSecondsCompleted(int secondsCompleted, ArrayList<Zielwert> zielwerte) {
-		// TODO komplett weiche Implementierung, das kann nur unter starker M??digkeit passiert sein
-		
-		  int level=0;
-		  int secondsCounter = zielwerte.get(0).getSeconds();
+	int getLevelFromSecondsCompleted(int secondsCompleted,
+			ArrayList<Zielwert> zielwerte) {
+		// TODO komplett weiche Implementierung, das kann nur unter starker
+		// M??digkeit passiert sein
 
-		  while (secondsCompleted > secondsCounter) {
-		    try {
-		      level++;
-//		      secondsCounter = secondsCounter + (Integer) zielwerteSeconds.get(level);
-		      secondsCounter = secondsCounter + zielwerte.get(level).getSeconds();
-		    } 
-		    catch (ArrayIndexOutOfBoundsException e) {
-		      // Seconds Completed h??her als irgendwas,
-		      // z??hle einfach Seconds Counter um 1 hoch, damit er fertig wird,
-		      // aber nicht level hochz??hlen!!
-		      secondsCounter = secondsCounter + 1;
-		    }
-		  }
+		int level = 0;
+		int secondsCounter = zielwerte.get(0).getSeconds();
 
-		  return level;
+		while (secondsCompleted > secondsCounter) {
+			try {
+				level++;
+				// secondsCounter = secondsCounter + (Integer)
+				// zielwerteSeconds.get(level);
+				secondsCounter = secondsCounter
+						+ zielwerte.get(level).getSeconds();
+			} catch (ArrayIndexOutOfBoundsException e) {
+				// Seconds Completed h??her als irgendwas,
+				// z??hle einfach Seconds Counter um 1 hoch, damit er fertig
+				// wird,
+				// aber nicht level hochz??hlen!!
+				secondsCounter = secondsCounter + 1;
+			}
 		}
+
+		return level;
+	}
 
 	/*
 	 * Executes local script to save sensor values into String file
-	 * 
 	 */
 	public void readSerialFromArduino() {
 		// TODO Auto-generated method stub
-//		ProcessBuilder pb = new ProcessBuilder("/home/pi/sensordata/readSerialFromArduino.sh");
-//		ProcessBuilder pb = new ProcessBuilder("/home/pi/sensordata/readSerialFromArduino.py");
+		// ProcessBuilder pb = new
+		// ProcessBuilder("/home/pi/sensordata/readSerialFromArduino.sh");
+		// ProcessBuilder pb = new
+		// ProcessBuilder("/home/pi/sensordata/readSerialFromArduino.py");
 		ProcessBuilder pb = null;
 		try {
 			Process p = pb.start();
@@ -234,7 +279,7 @@ public class HelloService {
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
-			
+
 		}
 	}
 
--- reaktor/src/com/andremotz/reaktor/servlets/ReaktorSessionServlet.java
+++ reaktor/src/com/andremotz/reaktor/servlets/ReaktorSessionServlet.java
@@ -15,33 +15,32 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ReaktorSession
  */
-@WebServlet("/ReaktorSession")
-public class ReaktorSession extends HttpServlet {
+@WebServlet("/")
+public class ReaktorSessionServlet extends HttpServlet {
 	
-	HelloService helloService;
+	ReaktorSessionService reaktorSessionService;
 	
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ReaktorSession() {
+    public ReaktorSessionServlet() {
         super();
         // TODO Auto-generated constructor stub
-        helloService = new HelloService();
+        reaktorSessionService = new ReaktorSessionService();
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		response.setContentType("text/plain");
 		PrintWriter writer = response.getWriter();
-		helloService.readSerialFromArduino();
-		helloService.circuitGo();
+		reaktorSessionService.readSerialFromArduino();
+		reaktorSessionService.circuitGo();
 		
 		String bgColor = "333333";
-		if (helloService.getSensorsTemps().get(0) == 0) {
+		if (reaktorSessionService.getSensorsTemps().get(0) == 0) {
 			bgColor = "FF0000";
 		} else {
 			bgColor = "FFFFFF";
@@ -49,7 +48,7 @@ public class ReaktorSession extends HttpServlet {
 		
 		String PAGE_HEADER = "<html><head>" +
 				"<meta charset='utf-8'> " +
-				"<meta http-equiv='refresh' content='" + String.valueOf(helloService.getGlobalTakt()) + "'>" +
+				"<meta http-equiv='refresh' content='" + String.valueOf(reaktorSessionService.getGlobalTakt()) + "'>" +
 				
 //				"<link rel=\"stylesheet\" href=\"http://code.jquery.com/ui/1.9.0/themes/base/jquery-ui.css\" />" + 
 //				"<script src=\"http://code.jquery.com/jquery-latest.min.js\" type=\"text/javascript\"></script>" + 
@@ -62,30 +61,30 @@ public class ReaktorSession extends HttpServlet {
 
 		writer.println(PAGE_HEADER);
 		writer.println("<h1>Reaktor Schaltzentrale</h1>");
-		writer.println(helloService.createHelloMessage("World"));
+		writer.println("Held der Brauerei!");
 
 		List<Float> sensorsTemps = new ArrayList<Float>();
 		
 		try {
-			sensorsTemps = helloService.getSensorsTemps();
+			sensorsTemps = reaktorSessionService.getSensorsTemps();
 		} catch (IndexOutOfBoundsException e) {
 			sensorsTemps.add(0f);
 			sensorsTemps.add(0f);
 		}
 		writer.println("<h2>Aktuelle Messwerte</h2>");
-		writer.println("<p>Sensorwerte: " + helloService.getSensorsAverage() + "</p>");
+		writer.println("<p>Sensorwerte: " + reaktorSessionService.getSensorsAverage() + "</p>");
 		writer.println("<ul>" + "<li>S1 Celsius: " + sensorsTemps.get(0)
 				+ "<li>S2 Celsius: " + sensorsTemps.get(1) + "</ul>");
 		
 
 		writer.println("<ul>" + "<li>comment: "
-				+ helloService.getGlobalData().getComment() + "</li> "
+				+ reaktorSessionService.getGlobalData().getComment() + "</li> "
 				+ "<li>isRunning: "
-				+ String.valueOf(helloService.getGlobalData().getIsRunning()) + "</li> " 
+				+ String.valueOf(reaktorSessionService.getGlobalData().getIsRunning()) + "</li> " 
 				+ "<li>secondsCompleted: "
-				+ String.valueOf(helloService.getGlobalData().getSecondsCompleted()) + "</li> " 
-				+ "<li>is heating: " + String.valueOf(helloService.getIsHeating()) + "</li>" +
-				"<li>Aktuelle Zieltemperatur: " + String.valueOf(helloService.getCurrentZieltemp()) + "</li>" +
+				+ String.valueOf(reaktorSessionService.getGlobalData().getSecondsCompleted()) + "</li> " 
+				+ "<li>is heating: " + String.valueOf(reaktorSessionService.getIsHeating()) + "</li>" +
+				"<li>Aktuelle Zieltemperatur: " + String.valueOf(reaktorSessionService.getCurrentZieltemp()) + "</li>" +
 				"</ul>");
 
 		writer.println(PAGE_FOOTER);
